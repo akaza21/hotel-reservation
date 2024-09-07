c8cf9f6088d6fdbafe3ae1d5fd290c413d2f00d9
akaza21 <pragyash20_ug@ece.nits.ac.in>
2024-09-07 18:59:08 +0530

    CRUD

--- types/user.go
+++ types/user.go
@@ -1,7 +1,99 @@
 package types
 
+import (
+	"fmt"
+	"regexp"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const ( 
+	bcryptCost = 12
+	minFirstNameLen = 2
+	minLastNameLen = 2
+	minPasswordLen = 8
+) 
+
+
+type CreateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	EMail     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+type UpdateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	EMail     string `json:"email"`
+}
+
+func (p UpdateUserParams) ToBSON() bson.M {
+	m:=bson.M{}
+	if len(p.FirstName) > 0 {
+		m["firstName"] = p.FirstName
+	}
+	if len(p.LastName) > 0 {
+		m["lastName"] = p.LastName
+	}
+	return m 
+}
+
+func (params *UpdateUserParams) UpdateValidate() map[string]string {
+	errors := map[string]string{}
+	if len(params.FirstName) < minFirstNameLen {
+		errors["firstName"] = fmt.Sprintf("firstName must be at least %d characters", minFirstNameLen)
+	}
+	if len(params.LastName) < minLastNameLen {
+		errors["lastName"] = fmt.Sprintf("lastName must be at least %d characters", minLastNameLen)
+	}
+	if !isEMailValid(params.EMail) {
+		errors["email"] = "email is not valid"
+	}
+	return errors
+}
+
+func (params *CreateUserParams) Validate() map[string]string {
+	errors := map[string]string{}
+	if len(params.FirstName) < minFirstNameLen {
+		errors["firstName"] = fmt.Sprintf("firstName must be at least %d characters", minFirstNameLen)
+	}
+	if len(params.LastName) < minLastNameLen {
+		errors["lastName"] = fmt.Sprintf("lastName must be at least %d characters", minLastNameLen)
+	}
+	if len(params.Password) < minPasswordLen {
+		errors["password"] = fmt.Sprintf("password must be at least %d characters", minPasswordLen)
+	}
+	if !isEMailValid(params.EMail) {
+		errors["email"] = "email is not valid"
+	} 
+	return errors
+}
+
+func isEMailValid(email string) bool {
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return emailRegex.MatchString(email)
+}
+
 type User struct {
-	ID                string 			 `bson:"_id" json:"id,omitempty"`
-	FirstName         string             `bson:"firstName" json:"firstName"`
-	LastName          string             `bson:"lastName" json:"lastName"`
+	ID                primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	FirstName         string `bson:"firstName" json:"firstName"`
+	LastName          string `bson:"lastName" json:"lastName"`
+	EMail             string `bson:"email" json:"email"`
+	EncryptedPassword string `bson:"encryptedPassword" json:"-"`
+}
+
+func NewUserFromParams(params *CreateUserParams) (*User, error) {
+	encpw, err := bcrypt.GenerateFromPassword([]byte(params.Password), bcryptCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		FirstName:         params.FirstName,
+		LastName:          params.LastName, 
+		EMail:             params.EMail,
+		EncryptedPassword: string(encpw),
+	}, nil
 }
